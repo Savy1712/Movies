b58690ad6ec982eb785256535a1f8abc918def0b
Sarveshwaran Rajarajan <savy.1712@gmail.com>
2018-01-18 22:08:34 +1100

    Aligning the video file name in PlayVideo.php

--- php/PlayVideo.php
+++ php/PlayVideo.php
@@ -1,3 +1,7 @@
+<?php
+include "Paths.php";
+?>
+
 <html lang="en">
 <head> 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
@@ -18,11 +22,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 <body>
-<div id = "InnerBox">
 
- 
-<div id = "MovieName"><h1> <?php echo  $movie_name; ?></h1> </div>
-<video controls width="1280" height="720" >
+<div class = "Enclose"> 
+<table>
+<tr>
+<td><img src="<?php echo $DEFAULT_LOCATION.'back.png'; ?>" width="100" height="100" onclick="Close()" /></td>
+<td width="100%"> <div class="MovieName"> <h1> <?php echo  $movie_name; ?></h1> </div> </td>
+</tr>
+</table>
+</div>
+<div id = "InnerBox">
+<video controls width="640" height="480" >
 <source src="<?php echo $movie_path ?>" >
 </video>
 </div>
